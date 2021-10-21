cdb70fe0e2a0495a5eae4fd070049379db3ce582
dwendt <dwendt@users.noreply.github.com>
2021-10-20 17:28:11 -0700

    Expose support for accessing YARA meta-fields.

--- libyara.NET/Rule.h
+++ libyara.NET/Rule.h
@@ -16,6 +16,7 @@ namespace libyaraNET {
     public:
         property String^ Identifier;
         property List<String^>^ Tags;
+        property List<Tuple<String^, String^>^>^ Metas;
 
         /// <summary>
         /// Create an empty Rule. Useful for testing.
@@ -36,6 +37,16 @@ namespace libyaraNET {
             yr_rule_tags_foreach(rule, tag)
             {
                 Tags->Add(marshal_as<String^>(tag));
+            }
+
+            yr_rule_metas_foreach(rule, meta)
+            {
+                // Meta fields have two main strings. The name ("identifier"), and what it's equal to ("string").
+                Tuple<String^, String^>^ storeme =
+                    gcnew Tuple<String^, String^>(
+                        marshal_as<String^>(meta->identifier),
+                        marshal_as<String^>(meta->string));
+                Metas->Add(storeme);
             }
         }
     };
