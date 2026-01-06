28c055beabefacbeee6907b2ce126825482d57b6
henriblMSFT <62611385+henriblMSFT@users.noreply.github.com>
2026-01-06 08:54:05 -0800

    Upgrade to .net 8.0 (#49)
    Co-authored-by: henribl <henribl+odspmdb@microsoft.com>

--- GenerateRefAssemblies.ps1
+++ GenerateRefAssemblies.ps1
@@ -13,7 +13,7 @@ if (Test-Path ".\ref") {
 
 $platforms = @("x64", "Win32", "ARM64")
 $configurations = @("Debug", "Release")
-$targetFrameworks = @("net6.0", "net462")
+$targetFrameworks = @("net8.0", "net462")
 $targetAssemblyName = "libyara.NET.dll"
 
 $generated = @()
@@ -26,7 +26,7 @@ $platforms | ForEach-Object {
             $targetAssembly = ".\$platform\$configuration\$targetFramework\$targetAssemblyName"
             if (($generated -notcontains $targetFramework) -and (Test-Path $targetAssembly)) {
                 Write-Host "Generating reference assembly for $targetAssembly"
-                & $refasmer -v -O "ref\$targetFramework" -c $targetAssembly
+                & $refasmer -v --omit-non-api-members=false -O "ref\$targetFramework" -c $targetAssembly
                 if (Test-Path ".\ref\$targetFramework\$targetAssemblyName") {
                     Write-Host "Reference assembly for $targetFramework generated successfully."
                     $generated += $targetFramework
